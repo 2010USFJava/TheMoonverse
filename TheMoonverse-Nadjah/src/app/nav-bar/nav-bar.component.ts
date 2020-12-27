cf581c388b3f3ee3423548e3eb73db45cc765447
unknown <dbond.tech@gmail.com>
2020-12-27 15:12:38 -0600

    reset from dev

---
+++ TheMoonverse-Nadjah/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {NavBarService } from '../nav-bar.service';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor(public nav: NavBarService) { }
+
+  ngOnInit(): void {
+  }
+
+}
